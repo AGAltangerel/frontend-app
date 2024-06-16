a2f6e818e0462d74ee3a9ee52bbdb22d75732cad
AG <aagiihan@gmail.com>
2024-06-16 16:59:57 +0900

    nuxt config file was modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,34 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
-  dev: process.env.NODE_ENV !== 'production',
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    dev: process.env.NODE_ENV !== 'production',
+    devtools: { enabled: true },
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
     },
-  },
-  router: {
-    middleware: ['auth'], // Ensure it's an array
-  },
-  ui: {
-    container: {
-      constrained: 'max-w-5xl'
-    }
-  },
-  buildModules: [
-    '@nuxt/typescript-build',
-    '@nuxtjs/tailwindcss',
-  ],
-  plugins: ['~/plugins/axios.ts'],
-  components: true,
-  build: {
-    transpile: ['/^@nuxt/'],
-  },
+    server: {
+        port: process.env.PORT || 80,
+        host: '0.0.0.0',
+    },
+    router: {
+        middleware: ['auth'], // Ensure it's an array
+    },
+    ui: {
+        container: {
+            constrained: 'max-w-5xl'
+        }
+    },
+    buildModules: [
+        '@nuxt/typescript-build',
+        '@nuxtjs/tailwindcss',
+    ],
+    plugins: ['~/plugins/axios.ts'],
+    components: true,
+    build: {
+        transpile: ['/^@nuxt/'],
+    },
 })
